ec22252efc559fca89f0287b9c58f58cc21446aa
Jun Hu <cbc106008@nptu.edu.tw>
2020-04-07 14:32:49 +0800

    Mean_shift 2.0 version.

--- mean_shift.py
+++ mean_shift.py
@@ -9,9 +9,14 @@ cluster_shape = (n_cluster_points, point_dim)
 
 # Randomly generate clusters using Normal Distribution (randn)
 rand_points = 0 + 2 * np.random.randn(*cluster_shape)
+rand_points2 = 10 + 3 * np.random.randn(*cluster_shape)
+rand_points3 = [20, 0] + 2 * np.random.randn(*cluster_shape)
+
+all_points = np.concatenate((rand_points, rand_points2, rand_points3), axis=0)
 
 # We only need random point rather than cluster so using uniform distribution.
-centroid = 10 * np.random.rand(point_dim) - 5
+# centroid = 10 * np.random.rand(point_dim) - 5
+centroid = np.copy(rand_points[0])
 
 
 def neighborhood_points(xs, x_centroid, dist=3):
@@ -27,20 +32,20 @@ def neighborhood_points(xs, x_centroid, dist=3):
 iteration = 0
 while True:
 
-    plt.scatter(rand_points[:, 0], rand_points[:, 1], color="blue", s=50, alpha=0.1)
+    plt.scatter(all_points[:, 0], all_points[:, 1], color="blue", s=50, alpha=0.1)
 
-    plt.scatter(*centroid, color="red", marker="+", s=200)
+    plt.scatter(*centroid, color="red", s=50)
     eligible_points = neighborhood_points(rand_points, centroid)
-    plt.scatter(eligible_points[:, 0], eligible_points[:, 1], color="green", s=50, alpha=0.1)
-    new_centroid = np.average(eligible_points, axis=0)
-    plt.scatter(*new_centroid, color="red", marker="*", s=200)
+    # plt.scatter(eligible_points[:, 0], eligible_points[:, 1], color="green", s=50, alpha=0.1)
+    new_centroid = np.mean(eligible_points, axis=0)
+    # plt.scatter(*new_centroid, color="red", marker="*", s=200)
 
     # Record distance between new and old centroid in oder to determine convergence.
     centroids_distant = np.linalg.norm(new_centroid - centroid)
-    plt.title("iteration %s, Centroids distant=%s" % (iteration, centroids_distant))
+    plt.title("iteration %s, Centroids distant=%.4f" % (iteration, centroids_distant))
 
     plt.draw()
-    plt.pause(0.2)
+    plt.pause(0.5)
     iteration += 1
 
     # We assume converged when centroid no more updated that same as k-means.
