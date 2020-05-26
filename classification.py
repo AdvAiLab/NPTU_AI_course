b9807cd91509598975073e0bbe46778c139cd8f7
Jun Hu <cbc106008@stmail.nptu.edu.tw>
2020-05-26 15:06:46 +0800

    Use predict result.

--- classification.py
+++ classification.py
@@ -24,9 +24,13 @@ colors = ["C%d" % l for l in labels]
 y_train = keras.utils.to_categorical(labels, num_classes=3)
 print(y_train.shape)
 
-plt.scatter(*data.T, alpha=0.5, c=colors)
+plt.scatter(*data.T, alpha=0.5, c=colors, s=50)
+predict_scat = plt.scatter([],[], alpha=0.5, marker="+", s=50)
+
+plt.xlim(1.5, -1.5)
+plt.ylim(1.5, -1.5)
 plt.grid()
-plt.show()
+# plt.show()
 
 model = Sequential()
 model.add(Dense(7, input_shape=(2,), activation="tanh"))
@@ -38,6 +42,21 @@ model.summary()
 model.compile(loss='categorical_crossentropy',
               optimizer='sgd',
               metrics=['accuracy'])
-history = model.fit(data, y_train,
-                    batch_size=1,
-                    epochs=10)
+epochs = 20
+
+for ep in range(epochs):
+    history = model.fit(data, y_train,
+                        batch_size=1,
+                        epochs=1)
+    test_data = np.random.rand(1000, 2)
+    test_data *= 3
+    test_data -= 1.5
+    plt.title("ep: %d" % ep)
+    y_predict = model.predict(test_data)
+    labels = np.argmax(y_predict, axis=1)
+    colors = ["C%d" % l for l in labels]
+    predict_scat.set_offsets(test_data)
+    predict_scat.set_color(colors)
+    plt.pause(0.5)
+    plt.draw()
+plt.show()
